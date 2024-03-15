fa32f030986f2d3375a91ed6f91e2451a59c51f6
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-03-15 14:50:16 -0300

    feat: first commit

---
+++ src/main/java/org/reis/itacademychallenge/ItacademyChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.reis.itacademychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItacademyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItacademyChallengeApplication.class, args);
+	}
+
+}
