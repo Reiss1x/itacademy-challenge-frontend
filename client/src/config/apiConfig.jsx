0f23d004393eb3eb48e1c559660fb39c02e5bb14
Reiss1x <gabrielreis.reis1903@gmail.com>
2024-04-29 16:21:28 -0300

    fix: fixes for running locally

--- client/src/config/apiConfig.jsx
+++ client/src/config/apiConfig.jsx
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://itacademy-challenge-production.up.railway.app/"
-// export const API_BASE_URL = "http://localhost:8080/"
+// export const API_BASE_URL = "http://18.116.82.192:8080/"
+export const API_BASE_URL = "http://localhost:8080/"
